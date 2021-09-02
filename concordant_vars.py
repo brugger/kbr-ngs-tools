1a9ec482bf774137dedecb749b227e613e2e3903
brugger <kim.brugger@uib.no>
2021-09-02 09:59:38 +0200

    fixes

--- concordant_vars.py
+++ concordant_vars.py
@@ -15,6 +15,9 @@ import pprint as pp
 from pysam import VariantFile, TabixFile
 
 
+def print_stderr(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
 def main() -> None:
 
 
@@ -27,6 +30,7 @@ def main() -> None:
     parser.add_argument('-s', '--snps-only', help="Only consider SNPS ", default=True, action='store_false')
     parser.add_argument('-m', '--multi-vars', help="add multi variant sites to analysis  ", default=False, action='store_true')
     parser.add_argument('-r', '--reps-file', help='tab file with rep regions (mq=0)')
+    parser.add_argument('-v', '--vcf-out', help='print interesting vars to stdout', default=False, action='store_true')
     parser.add_argument('infile', nargs=1, help="file to analyse")
 
     args = parser.parse_args()
@@ -50,15 +54,9 @@ def main() -> None:
         tbx = TabixFile(args.reps_file)
 
     vcf_in = VariantFile(args.infile)  # auto-detect input format
-#    print( vcf_in.header, end='' )
+    if args.vcf_out:
+        print( vcf_in.header, end='' )
     for rec in vcf_in.fetch(args.chrom):
-#        print( rec, end='' )
-
-        if  len( rec.alts ) > 1 and args.multi_vars == False:
-            continue
-
-        if  len( rec.alts[0]) > 1:
-            continue
 
         if rec.qual < int( args.quality ):
             low_quality += 1
@@ -78,6 +76,7 @@ def main() -> None:
             low_depth += 1
             continue
 
+
         gq_low = False
         for gq in [rec.samples[s]['GQ'] for s in rec.samples ]:
             if gq is not None and gq < int(args.gq):
@@ -87,6 +86,7 @@ def main() -> None:
 
         if gq_low:
             continue
+
 
         if tbx is not None:
             rows = tbx.fetch(rec.chrom, rec.pos, rec.pos+1)
@@ -98,14 +98,27 @@ def main() -> None:
 
         if rec.samples[0]['GT'] == rec.samples[1]['GT']:
             concordant += 1
+            continue
+
+        if  len( rec.alts ) > 1 and args.multi_vars == False:
+#            print( f"ODD\t{rec.chrom}:{rec.pos}\t{rec.ref}\t{rec.alts}\t{rec.qual}\t{rec.samples[0]['GT']} != {rec.samples[1]['GT']}" )
+            if args.vcf_out:
+                print( rec, end='' )
+
+            continue
+
         elif None in rec.samples[0]['GT'] + rec.samples[1]['GT']:
             no_call += 1
         elif rec.samples[0]['GT'] == (1,1) and  rec.samples[1]['GT'] == (0,1):
-            print( f"LOST\t{rec.chrom}:{rec.pos}\t{rec.qual}\t{rec.samples[0]['GT']} != {rec.samples[1]['GT']}" )
+#            print( f"LOST\t{rec.chrom}:{rec.pos}\t{rec.qual}\t{rec.samples[0]['GT']} != {rec.samples[1]['GT']}" )
+            if args.vcf_out:
+                print( rec, end='' )
             lost_allele +=1
             discordant += 1
         elif rec.samples[0]['GT'] == (0,0):
-            print( f"GAIN\t{rec.chrom}:{rec.pos}\t{rec.qual}\t{rec.samples[0]['GT']} != {rec.samples[1]['GT']}" )
+#            print( f"GAIN\t{rec.chrom}:{rec.pos}\t{rec.qual}\t{rec.samples[0]['GT']} != {rec.samples[1]['GT']}" )
+            if args.vcf_out:
+                print( rec, end='' )
             new_allele +=1
             discordant += 1
 
@@ -113,9 +126,9 @@ def main() -> None:
             discordant += 1
 
 
-    print(f"total: {total_vars}, concordant: {concordant}, discordant: {discordant}")
-    print(f"Filtered out: low-depth: {low_depth}, no_call: {no_call}, rep_regions: {rep_region}, low_gq: {low_gq}, low quality: {low_quality}, lost_allele: {lost_allele}, new_allele: {new_allele}")
-    print(f"corcondance: {(concordant*100)/(discordant+concordant)} %")
+    print_stderr(f"total: {total_vars}, concordant: {concordant}, discordant: {discordant}")
+    print_stderr(f"Filtered out: low-depth: {low_depth}, no_call: {no_call}, rep_regions: {rep_region}, low_gq: {low_gq}, low quality: {low_quality}, lost_allele: {lost_allele}, new_allele: {new_allele}")
+    print_stderr(f"corcondance: {(concordant*100)/(discordant+concordant)} %")
 
 
 if __name__ == "__main__":
